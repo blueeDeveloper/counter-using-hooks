045cd78473fa23073b2b40b7b5a17bdd74c0bd21
Ashwin Prabhu <ashwin.msprabhu@gmail.com>
2021-02-26 00:03:52 -0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
+import { Button, ButtonGroup } from 'react-bootstrap';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const changeCounter = (direction) => {
+    direction === "increment" ? setCount(count => count+ 1) : setCount(count => count- 1)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        { count }
+        <ButtonGroup>
+        <Button variant="primary" onClick={() => changeCounter("increment")}>Increment count</Button>
+        <Button style={{ marginLeft: 10}} variant="primary" onClick={() => changeCounter("decrement")}>Decrement count</Button>
+        </ButtonGroup>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Ashwin
         </a>
       </header>
     </div>
